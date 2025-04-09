4a073e5de97f109a024470cf20df0589060d16aa
sindhukanagaraju <sindhuanagaraju@gmail.com>
2025-04-09 17:17:44 +0530

    search updated

--- src/main/java/com/showroommanagement/ShowroomManagementApplication.java
+++ src/main/java/com/showroommanagement/ShowroomManagementApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShowroomManagementApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(ShowroomManagementApplication.class, args);
+public class ShowroommanagementApplication {
+    public static void main(String[]args){
+        SpringApplication.run(ShowroommanagementApplication.class,args);
     }
 }
